7d06288b380686b215a73846bc3484ef2262ef1d
Hadrien Renaud <hadrien.renaud2@arm.com>
2024-07-08 07:53:48 +0100

    [asl] [tool] Build a big file with instructions
    This commit introduces a new option, `-m` or `--make-functions`, which
    process all the files passed in input, and for all instruction files,
    write a corresponding function in the output file `instructions.asl`.
    
    Compared to previous features of this script, this feature build ASL
    code corresponding to the decode part of the instruction. Indeed, the
    built function has only take a `bits(32)` type as input: the
    instruction in binary format.
    
    We currently extract only one decode section per instruction.

--- herd/libdir/asl-pseudocode/bundler.py
+++ herd/libdir/asl-pseudocode/bundler.py
@@ -6,13 +6,14 @@ Working assumptions:
 """
 
 import argparse
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime
 import itertools
-from concurrent.futures import ThreadPoolExecutor
 import logging
-from xml.etree.ElementTree import Element, parse
 from pathlib import Path
 from typing import List, Optional
-from datetime import datetime
+import textwrap
+from xml.etree.ElementTree import Element, parse
 
 # In this whole module, path names used as input (resp. output) are prefixed with `i_` (resp. `o_`).
 
@@ -38,6 +39,10 @@ MESSAGE_ON_TOP = """
 """
 
 DEFAULT_INSTR_DIR = Path("other-instrs")
+
+SEPARATOR_LINE = (
+    "// ============================================================================="
+)
 
 
 def o_path_of_tree(root: Element, o_dir: Path) -> Path:
@@ -98,7 +103,7 @@ def header_of_tree(root: Element, o_path: Path) -> str:
     return "\n".join(
         (
             *("// {:^74}".format(i) for i in titles),
-            "// =============================================================================",
+            SEPARATOR_LINE,
             "",
             MESSAGE_ON_TOP,
             *post_header,
@@ -107,22 +112,140 @@ def header_of_tree(root: Element, o_path: Path) -> str:
     )
 
 
+def asl_for_instruction_fields(root) -> str:
+    """Reads the xml for an instruction and constructs the ASL from it's
+    tabular representation of the instruction binary encoding"""
+
+    result = []
+
+    path = ".//classes/iclass/regdiagram"
+    nodes = root.findall(path)
+    if len(nodes) == 0:
+        _logger.warning("Could not find node at %s", path)
+        return []
+    if len(nodes) >= 2:
+        _logger.warning("Too many nodes matching path %s", path)
+    node = nodes[0]
+
+    for b in node.findall("./box"):
+        name = b.get("name")
+        if name is None:
+            continue
+
+        name = name.strip()
+        if not name.isidentifier():
+            _logger.warning("Ignoring uncompatible field name '%s'", name)
+            continue
+
+        hibit = b.get("hibit")
+        width = b.get("width")
+
+        pos = hibit if width is None else f"{hibit} : ({hibit} - {width} + 1)"
+        result.append(f"let {name} = instruction[{pos}];")
+
+    return result
+
+
+def read_text_in_node(root, path, warn=True) -> [str]:
+    """Reads all the text at that path"""
+    nodes = root.findall(path)
+    if len(nodes) == 0:
+        if warn:
+            _logger.warning("Could not find node at %s", path)
+        return ""
+    if len(nodes) >= 2 and warn:
+        _logger.warning("Too many nodes matching path %s", path)
+
+    return "".join(nodes[0].itertext())
+
+
+def one_instruction_to_string(i_path: Path) -> str:
+    """Process one instruction, write the decode and write it to output directory"""
+    _logger.info("Processing %s", i_path)
+
+    root = parse(i_path).getroot()
+
+    if root.tag != "instructionsection":
+        _logger.error("Cannot interpret file %s -- Skipping.", i_path)
+        return ""
+
+    root_type = root.get("type")
+    if root_type == "pseudocode":
+        _logger.info("Skipping shared pseudocode at %s", i_path)
+        return ""
+    if root_type == "alias":
+        _logger.info("Skipping alias at %s.", i_path)
+        return ""
+    if root_type != "instruction":
+        _logger.warning("Unknown xml type at %s -- Skipping", i_path)
+        return ""
+
+    operation = read_text_in_node(root, ".//ps_section/ps/pstext[@section='Execute']")
+    decode = read_text_in_node(root, ".//ps_section/ps/pstext[@section='Decode']")
+    post_decode = read_text_in_node(
+        root, ".//ps_section/ps/pstext[@section='Postdecode']", warn=False
+    )
+    from_bin = asl_for_instruction_fields(root)
+
+    f_name = "instruction_code_" + i_path.stem
+    instruction_name = root.get(
+        "title", default="Instruction without a name (could not parse it from the xml)"
+    )
+
+    function_content = "\n".join(
+        (
+            "// begining of binary unpacking",
+            *from_bin,
+            "// end of binary unpacking",
+            "",
+            "// begining of decode section",
+            decode,
+            "// end of decode section",
+            "",
+            "// begining of post decode section",
+            post_decode,
+            "// end of post decode section",
+            "",
+            "// begining of execute section",
+            operation,
+            "// end of execute section",
+        )
+    )
+
+    text = "\n".join(
+        (
+            SEPARATOR_LINE,
+            f"// {instruction_name}",
+            SEPARATOR_LINE,
+            "",
+            "" f"func {f_name} (instruction: bits(32))",
+            "begin",
+            textwrap.indent(function_content, "  "),
+            "end",
+            "",
+            "",
+        )
+    )
+
+    return text
+
+
 def process_one_file(i_file: Path, o_dir: Path):
     """Process one file and write it to output directory."""
-    _logger.info(f"Processing {i_file}")
+    _logger.info("Processing %s", i_file)
 
     root = parse(i_file).getroot()
 
     if root.tag != "instructionsection":
-        _logger.error(f"Cannot interpret file {i_file} -- Skipping.")
+        _logger.error("Cannot interpret file %s -- Skipping.", i_file)
         return
 
-    elif root.get("type") == "alias":
-        _logger.info(f"Skipping alias at {i_file}.")
+    if root.get("type") == "alias":
+        _logger.info("Skipping alias at %s.", i_file)
         return
 
     o_path = o_path_of_tree(root, o_dir)
-    _logger.info(f"Writing to {o_path}")
+    _logger.info("Writing to %s", o_path)
 
     header = header_of_tree(root, o_path)
     with o_path.open("w") as f:
@@ -130,14 +253,14 @@ def process_one_file(i_file: Path, o_dir: Path):
 
         for ps in root.findall("./ps_section/ps"):
             sect_type = ps.get("secttype")
-            if sect_type != "Library" and sect_type != "Operation":
+            if sect_type not in ("Library", "Operation"):
                 continue
 
-            _logger.debug(f"Writing section {ps.get('name')}")
+            _logger.debug("Writing section %s", ps.get("name"))
             f.writelines(ps.find("pstext").itertext())
             f.write("\n\n")
 
-    _logger.debug(f"Processed {i_file}")
+    _logger.debug("Processed %s", i_file)
 
 
 def get_args() -> (List[Path], Path, Optional[int]):
@@ -159,8 +282,8 @@ def get_args() -> (List[Path], Path, Optional[int]):
         metavar="PATH",
         type=Path,
         nargs="+",
-        help="The different paths to parse. If this is a directory, this will (non-recursively) parse all files inside"
-        " the directory that have the '.xml' extension.",
+        help="The different paths to parse. If this is a directory, this will (non-recursively) "
+        "parse all files inside the directory that have the '.xml' extension.",
     )
     logger_group = parser.add_mutually_exclusive_group()
     logger_group.add_argument(
@@ -183,8 +306,14 @@ def get_args() -> (List[Path], Path, Optional[int]):
         "--jobs",
         action="store",
         type=int,
-        help="Parallelization on parsing and writing jobs. Default to python's ThreadPoolExecutor default, which"
-        " should be `min(32, cpu_count)`",
+        help="Parallelization on parsing and writing jobs. Default to python's ThreadPoolExecutor"
+        "default, which  should be `min(32, cpu_count)`",
+    )
+    parser.add_argument(
+        "-m",
+        "--make-functions",
+        action="store_true",
+        help="Make function and not opns.",
     )
 
     args = parser.parse_args()
@@ -211,37 +340,56 @@ def get_args() -> (List[Path], Path, Optional[int]):
     for f in args.paths:  # type: Path
         f = f.expanduser()
         if f.is_dir():
-            _logger.info(f"Extending directory {f}")
+            _logger.info("Extending directory %s", f)
             i_files.extend(f.glob("*.xml"))
         elif f.exists():
             i_files.append(f)
         else:
-            _logger.warning("Ignoring " + str(f))
+            _logger.warning("Ignoring %s", f)
 
     o_dir = args.output.absolute()  # type: Path
     if not o_dir.exists():
         _logger.info("Output dir does not exist. Creating it.")
-        _logger.debug(f"mkdir {o_dir}")
+        _logger.debug("mkdir %s", o_dir)
         o_dir.mkdir(exist_ok=True, parents=True)
 
     elif not o_dir.is_dir():
-        _logger.error(f"Output option is not a directory. Might break later.")
+        _logger.error("Output option is not a directory. Might break later.")
 
     jobs = args.jobs
     if jobs is None:
         _logger.info("Starting process with default number of parallel workers.")
     else:
-        _logger.info(f"Starting process with {jobs} workers.")
+        _logger.info("Starting process with %d workers.", jobs)
 
-    return i_files, o_dir, jobs
+    make_functions = args.make_functions
+
+    return i_files, o_dir, jobs, make_functions
 
 
 def main():
     """Main entry point."""
-    (i_files, o_dir, jobs) = get_args()
+    (i_files, o_dir, jobs, make_functions) = get_args()
 
     with ThreadPoolExecutor(max_workers=jobs) as executor:
-        executor.map(process_one_file, i_files, itertools.repeat(o_dir, len(i_files)))
+
+        if make_functions:
+
+            future_strings = (
+                executor.submit(one_instruction_to_string, i_file) for i_file in i_files
+            )
+
+            o_file = o_dir / "instructions.asl"
+            with open(o_file, "w", encoding="utf8") as f:
+                f.write(MESSAGE_ON_TOP)
+
+                for future_string in as_completed(future_strings):
+                    f.write(future_string.result())
+
+        else:
+            executor.map(
+                process_one_file, i_files, itertools.repeat(o_dir, len(i_files))
+            )
 
 
 if __name__ == "__main__":
